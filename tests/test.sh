da4f8ea80f91a165310bbcdfee529905d4b45f26
Hzhao <hzhao@ocadu.ca>
2026-06-11 15:09:58 -0400

    The First Descendant Fix

--- tests/test.sh
+++
@@ -1,8 +0,0 @@
-# usr/bin/env bash
-# This file is meant to be executed from inside a Docker container.
-# To run tests on the host system, use the `test.sh` script in the root directory.
-export BLENDER_EXECUTABLE=$(cat /blender_executable_path)
-pytest --cov-report xml --cov=/root/.config/blender -svv tests --blender-executable $BLENDER_EXECUTABLE --blender-addons-dirs ../addons
-# Fixes the paths in the coverage report to be relative to the current directory.
-sed -i 's|/root/.config/blender||g' coverage.xml
-sed -i 's|5.0/scripts/addons/io_scene_psk_psa/||g' coverage.xml
